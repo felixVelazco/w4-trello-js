21b2d8168faf2d2fbb74aa2712f8bcc3a002d989
felix <josevelazco134@gmail.com>
2022-05-02 16:53:41 -0600

    add new functions with promises instead of callbacks

--- app.js
+++ app.js
@@ -7,15 +7,40 @@ if(!process.env.TOKEN && !process.env.KEY){
 let Trello = require("trello");
 let trello = new Trello(process.env.KEY, process.env.TOKEN);
 
-let cardTitle = `Card nueva ${new Date()}`;
+// let cardTitle = `Card nueva ${new Date()}`;
 
-trello.addCard(cardTitle, "LaunchX Card Description", "626dea5a90d6d6789af81917",
-  function(error, trelloCard){
-    if(error){
-      console.log("Could not add card", error);
-    }
-    else{
-      console.log("Added card", trelloCard);
-    }
-  }
-);
+// trello.addCard(cardTitle, "LaunchX Card Description", "626dea5a90d6d6789af81917",
+//   function(error, trelloCard){
+//     if(error){
+//       console.log("Could not add card", error);
+//     }
+//     else{
+//       console.log("Added card", trelloCard);
+//     }
+//   }
+// );
+
+// trello.getListsOnBoard("626dea5a90d6d6789af81916", "Ver algo",
+//   function(error, trelloList){
+//     if(error){
+//       console.log("Could not find any list", error);
+//     }
+//     else{
+//       console.log("Lists", trelloList);
+//     }
+//   }
+//   );
+let listId;
+let listsPromise = trello.getListsOnBoard("626dea5a90d6d6789af81916", "Ver listas en board");
+listsPromise.then((list) => {
+  listId = list[0];
+  createNewCard(`Nueva tarjeta ${new Date()}`, "Aqui va la descripcion", list[0].id)
+  console.log(listId.id);  
+})
+
+function createNewCard(nameCard, description, listId){
+  let createCard = trello.addCard(nameCard, description, listId);
+  createCard.then((card) => {
+    console.log("nueva Card creada!");
+  })
+}
